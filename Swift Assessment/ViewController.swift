2e24566743d9da605ad53833b899079f6aab483a
Victor De Leon <victor941@gmail.com>
2023-12-20 16:09:02 -0600

    added productViewMode, the View of tableView

--- Swift Assessment/ViewController.swift
+++ Swift Assessment/ViewController.swift
@@ -7,14 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    private var viewModel: ProductViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let result = sumOfEvenNumbers(numbers)
-        print("Sum of even numbers: \(result)")
+
+        view.backgroundColor = .white
+
+        setupViewModel()
+        setupTableView()
+        fetchProducts()
+    }
+
+    private func setupViewModel() {
+        viewModel = ProductViewModel()
+    }
+
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.register(ProductTableViewCell.self, forCellReuseIdentifier: "ProductCell")
+
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    private func fetchProducts() {
+        viewModel.fetchProducts { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfProducts
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ProductTableViewCell(style: .default, reuseIdentifier: "ProductCell")
+
+        if let product = viewModel.product(at: indexPath.row) {
+            cell.configure(with: product)
+        }
+
+        return cell
     }
 }
-
