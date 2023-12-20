fbcfd06c7b526b14efeaff5d39513a869c125bf6
Victor De Leon <victor941@gmail.com>
2023-12-20 15:21:07 -0600

    Initial Commit

---
+++ Swift Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Assessment
+//
+//  Created by VICTOR MANUEL DE LEON CHI on 20/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift AssessmentUITests/Swift_AssessmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_AssessmentUITestsLaunchTests.swift
+//  Swift AssessmentUITests
+//
+//  Created by VICTOR MANUEL DE LEON CHI on 20/12/23.
+//
+
+import XCTest
+
+final class Swift_AssessmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
