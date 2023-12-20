940b28ae54310c4b7af34eb09700402771615d68
Victor De Leon <victor941@gmail.com>
2023-12-20 16:09:36 -0600

    added the product object for match with API service

--- Swift Assessment/Excersice 2/ProductObject.swift
+++ Swift Assessment/Excersice 2/ProductObject.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+// MARK: - Product
+struct Product: Codable {
+    let products: [ProductElement]
+    let total, skip, limit: Int
+}
+
+// MARK: - ProductElement
+struct ProductElement: Codable {
+    let id: Int
+    let title, description: String
+    let price: Int
+    let discountPercentage, rating: Double
+    let stock: Int
+    let brand, category: String
+    let thumbnail: String
+    let images: [String]
+}
