cfd1197f34797281a00fce33f10566fcc9d93193
Victor De Leon <victor941@gmail.com>
2023-12-20 16:07:33 -0600

    added the excersice 2 and productObject for get the product JSON

--- Swift Assessment/Excersice 2/ProductService.swift
+++ Swift Assessment/Excersice 2/ProductService.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+enum ProductServiceError: Error {
+    case invalidURL
+    case networkError(Error)
+    case noData
+    case decodingError(Error)
+}
+
+enum ProductServiceResult {
+    case success(Product)
+    case failure(ProductServiceError)
+}
+
+class ProductService {
+    func fetchProducts(completion: @escaping (ProductServiceResult) -> Void) {
+        guard let url = URL(string: "https://dummyjson.com/products") else {
+            completion(.failure(.invalidURL))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(.networkError(error)))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+
+            do {
+                let products = try JSONDecoder().decode(Product.self, from: data)
+                completion(.success(products))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }.resume()
+    }
+}
