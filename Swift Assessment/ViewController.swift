c8dd8c4bea0dd27bd0546fb53d8098657cc6f23c
Victor De Leon <victor941@gmail.com>
2023-12-21 01:18:58 -0600

    delete uncesary code for excersi 5

--- Swift Assessment/ViewController.swift
+++ Swift Assessment/ViewController.swift
@@ -7,61 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
-
-    private var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        return tableView
-    }()
-
-    private var viewModel: ProductViewModel!
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
-
-        setupViewModel()
-        setupTableView()
-        fetchProducts()
     }
 
-    private func setupViewModel() {
-        viewModel = ProductViewModel()
-    }
-
-    private func setupTableView() {
-        tableView.dataSource = self
-        tableView.register(ProductTableViewCell.self, forCellReuseIdentifier: "ProductCell")
-
-        view.addSubview(tableView)
-
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-    }
-
-    private func fetchProducts() {
-        viewModel.fetchProducts { [weak self] in
-            self?.tableView.reloadData()
-        }
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numberOfProducts
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = ProductTableViewCell(style: .default, reuseIdentifier: "ProductCell")
-
-        if let product = viewModel.product(at: indexPath.row) {
-            cell.configure(with: product)
-        }
-
-        return cell
-    }
+ 
 }
