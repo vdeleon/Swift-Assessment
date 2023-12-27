c304065d35e95addff9c8b483391355335900d7d
Victor de leon <victor941@gmail.com>
2023-12-27 00:42:35 -0600

    delete unnecesary code

--- Swift Assessment/Excercise 5/LocationObject.swift
+++ Swift Assessment/Excercise 5/LocationObject.swift
@@ -5,11 +5,6 @@
 //  Created by VICTOR MANUEL DE LEON CHI on 20/12/23.
 //
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let locationObject = try? JSONDecoder().decode(LocationObject.self, from: jsonData)
-
 import Foundation
 
 // MARK: - LocationObject
